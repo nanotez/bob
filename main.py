2460240f2eaa493880f14c3a9ea3168ac4ae7d22
nanotez <97647355+nanotez@users.noreply.github.com>
2022-01-14 13:52:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,7 @@ while True:
  user = fake_useragent.UserAgent().random
  headers = {'user_agent': user}
  NUMBER = input('Ведите номер')
- 
-   try:
+  try:
       response = requests.post('https://my.telegram.org/auth/send_password', headers=headers, data={'phone' : "+" + NUMBER})
       print('Отправлен')
       except:
