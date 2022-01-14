9e06ed664827e0998ee51c5e965669f44fd9ac4b
nanotez <97647355+nanotez@users.noreply.github.com>
2022-01-14 14:03:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import requests, fake_useragent, time
 
+ user = fake_useragent.UserAgent().random
+ headers = {'user_agent': user}
 
 NUMBER = input('Ведите номер')
 
 while True:
  user = fake_useragent.UserAgent().random
  headers = {'user_agent': user}
- NUMBER = input('Ведите номер')
+ 
   try:
-      response = requests.post('https://rider.uklon.com.ua/api/v1/phone/send-code', headers=headers, data={'phone' : "+" NUMBER})
-      print('Отправлен')
-      except:
-        print('Не отправлен')
+			requests.post('https://rider.uklon.com.ua/api/v1/phone/send-code', data={'phone': NUMBER})
+			print('[+] Beltelcom отправлено!')
+		except:
+			print('[-] Не отправлено!')
+
         
        time.sleep(5)
       
